113b1a5694f982f54044a787d7fa9e344334baa3
Jose Ramos <engrjvramos@gmail.com>
2022-12-06 22:42:08 +0800

    setup workflow

---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ExpenseItem = (props) => {
+  return (
+    <>
+      <div>
+        <div>March 28, 2022</div>
+        <div>
+          <h2>Some Books</h2>
+          <div>$278.89</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseItem;
